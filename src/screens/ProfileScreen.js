9280792f45ea60bcbe1ec5dc5a21ee77438e9c18
Your Name <irfana.27@gmail.com>
2022-10-31 14:57:17 -0700

    updated stripe Subscription

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -17,6 +17,47 @@ function ProfileScreen() {
                     <h2>{user.email}</h2>
                     <div className="profileScreen__plans">
                         <h3>Plans(Current Plan: premium)</h3>
+                        <section>
+                            <div className="product">
+                                <div className="description">
+                                    <h3>Basic</h3>
+                                    <h5>$9.99</h5>
+                                </div>
+                                <form action="/create-checkout-session" method="POST">
+                                    {/* Add a hidden field with the lookup_key of your Price */}
+                                    <input type="hidden" name="lookup_key" value="4243" />
+                                    <button id="checkout-and-portal-button" type="submit">
+                                    Checkout
+                                    </button>
+                                </form>                                
+                            </div>
+                            <div className="product">
+                                <div className="description">
+                                    <h3>Standard</h3>
+                                    <h5>$14.89</h5>
+                                </div>
+                                <form action="/create-checkout-session" method="POST">
+                                    {/* Add a hidden field with the lookup_key of your Price */}
+                                    <input type="hidden" name="lookup_key" value="4244" />
+                                    <button id="checkout-and-portal-button" type="submit">
+                                    Checkout
+                                    </button>
+                                </form>                                
+                            </div>
+                            <div className="product">
+                                <div className="description">
+                                    <h3>PREMIUM</h3>
+                                    <h5>$19.99</h5>
+                                </div>
+                                <form action="/create-checkout-session" method="POST">
+                                    {/* Add a hidden field with the lookup_key of your Price */}
+                                    <input type="hidden" name="lookup_key" value="4245" />
+                                    <button id="checkout-and-portal-button" type="submit">
+                                    Checkout
+                                    </button>
+                                </form>                                
+                            </div>
+                        </section>
                         
                         <button className='profileScreen__signOut' onClick={()=>auth.signOut()}>Sign Out</button>
                     </div>
